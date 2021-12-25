40dff56071ff3bd1cf0d7fba2b8f611660e6b383
Reagan Turedi <60759353+ReaganTuredi@users.noreply.github.com>
2021-12-25 01:25:16 -0500

    Create template.cpp

---
+++ TemplateExample/template.cpp
@@ -0,0 +1,7 @@
+// This templated function returns its argument tripled. 
+
+#include <iostream>
+template <typename random_type>
+random_type tripled(random_type input) {
+return (input + input + input);
+}
